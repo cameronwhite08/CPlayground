b9f7ae6939c00ef65368836603462548d1c0bb30
Cameron White <cameronwhite08@gmail.com>
2019-01-12 09:36:40 -0500

    basic linked list functionality

---
+++ linkedlist/main.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int value;
+    struct node* next;
+};
+
+struct node* head;
+struct node* curr;
+
+struct node* createNode(int value){
+    struct node* newNode = (struct node*) malloc(sizeof(struct node));
+    newNode->value = value;
+    return newNode;
+}
+
+//currently walks entire list
+void insertLast(int value)
+{
+    curr = head;
+    //want to stay on last node in list
+    while(curr->next != NULL)
+    {
+        curr = curr->next;
+    }
+
+    struct node* newNode = createNode(value);
+    curr->next = newNode;
+}
+
+void insertFirst(int value)
+{
+    struct node* newNode = createNode(value);
+    newNode->next = head;
+
+    head = newNode;
+}
+
+void insertAfter(int value, struct node* start)
+{
+    struct node* newNode = createNode(value);
+    newNode->next = start->next;
+
+    start->next = newNode;
+}
+
+void printList(struct node* start)
+{
+    curr = start;
+    //walk entire list
+    while(curr != NULL)
+    {
+        printf("%d\t", curr->value);
+        curr = curr->next;
+    }
+}
+
+struct node* findNode(int key){
+    curr = head;
+
+    while(curr != NULL){
+        if(curr->value == key){
+            return curr;
+        }
+
+        curr = curr->next;
+    }
+    return NULL;
+}
+
+int main() {
+    //reset stdout for some reason??
+    setbuf(stdout, 0);
+
+    head = createNode(2);
+
+    insertLast(3);
+    insertLast(4);
+
+    insertFirst(0);
+
+    struct node* curr = findNode(0);
+    insertAfter(1, curr);
+
+    printList(head);
+
+    return 0;
+}
